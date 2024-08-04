757f3d569e4fd95c24b9c693cc4194dbdda200b7
kaitokimuraofficial <k.kaito0422@akane.waseda.jp>
2024-08-04 14:58:00 +0900

    Upgrade terraform/providers/aws

--- terraform.tf
+++ terraform.tf
@@ -2,8 +2,8 @@ terraform {
   required_version = ">=1.9"
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "5.47.0"
+      source = "hashicorp/aws"
+      version = "5.61.0"
     }
   }
 }
