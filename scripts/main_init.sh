7a7052c1a50d5f643a90a97d2191a78856473aeb
kaitokimuraofficial <k.kaito0422@akane.waseda.jp>
2024-08-10 09:09:35 +0900

    Auto-execute userdata

--- scripts/main_init.sh
+++ scripts/main_init.sh
@@ -1,7 +1,27 @@
+Content-Type: multipart/mixed; boundary="//"
+MIME-Version: 1.0
+
+--//
+Content-Type: text/cloud-config; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment; filename="cloud-config.txt"
+
+#cloud-config
+cloud_final_modules:
+- [scripts-user, always]
+
+--//
+Content-Type: text/x-shellscript; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment; filename="userdata.txt"
+
 #!/bin/bash
 
 set -e
 
-sudo dnf update -y
+dnf update -y
 
-sudo dnf -y install git vim mariadb105
+dnf -y install git vim mariadb105
+--//--
