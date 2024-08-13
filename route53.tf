c939da036c1bd33b76c2a8b0fdac2072aec33f92
kaitokimuraofficial <k.kaito0422@akane.waseda.jp>
2024-08-13 23:21:29 +0900

    Issued the certificate necessary for HTTPS

--- route53.tf
+++ route53.tf
@@ -1,3 +1,31 @@
 resource "aws_route53_zone" "my_domain" {
   name = var.domain_name
 }
+
+resource "aws_route53domains_registered_domain" "my_domain" {
+  domain_name = var.domain_name
+
+  dynamic "name_server" {
+    for_each = aws_route53_zone.my_domain.name_servers
+    content {
+      name = name_server.value
+    }
+  }
+}
+
+resource "aws_route53_record" "my_domain" {
+  for_each = {
+    for dvo in aws_acm_certificate.my_domain.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 300
+  type            = each.value.type
+  zone_id         = aws_route53_zone.my_domain.zone_id
+}
