b8f4850d3233759eaccbe1ad286bda80b1e70166
kaitokimuraofficial <k.kaito0422@akane.waseda.jp>
2024-08-21 07:41:13 +0900

    Add ECR

---
+++ ecr.tf
@@ -0,0 +1,45 @@
+resource "aws_ecr_repository" "blog" {
+  name                 = "blog"
+  image_tag_mutability = "IMMUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "blog" {
+  repository = aws_ecr_repository.blog.name
+
+  policy = jsonencode(
+    {
+      "rules" : [
+        {
+          "rulePriority" : 1,
+          "description" : "Keep last 10 images",
+          "selection" : {
+            "tagStatus" : "tagged",
+            "tagPrefixList" : ["dev"],
+            "countType" : "imageCountMoreThan",
+            "countNumber" : 10
+          },
+          "action" : {
+            "type" : "expire"
+          }
+        },
+        {
+          "rulePriority" : 2,
+          "description" : "Keep last 5 images",
+          "selection" : {
+            "tagStatus" : "tagged",
+            "tagPrefixList" : ["prod"],
+            "countType" : "imageCountMoreThan",
+            "countNumber" : 5
+          },
+          "action" : {
+            "type" : "expire"
+          }
+        }
+      ]
+    }
+  )
+}
