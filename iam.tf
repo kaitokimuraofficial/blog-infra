4c3515c7ee1c16c14be3b52352a12ed5d8a8f7d2
kaitokimuraofficial <k.kaito0422@akane.waseda.jp>
2024-09-03 17:12:44 +0900

    Add new policy to task execution role

--- iam.tf
+++ iam.tf
@@ -115,6 +115,11 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
   role       = aws_iam_role.ecs_task_execution_blog.name
 }
 
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_ec2_container_registry_read_only_role" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.ecs_task_execution_blog.name
+}
+
 resource "aws_iam_role" "ecs_task_blog" {
   name               = "ecs-task-blog"
   assume_role_policy = data.aws_iam_policy_document.ecs_task_assume_role.json
