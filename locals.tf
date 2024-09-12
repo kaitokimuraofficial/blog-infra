541c56619831ada2183bf086c75f244e4546b0d4
kaitokimuraofficial <k.kaito0422@akane.waseda.jp>
2024-09-13 07:39:18 +0900

    Delete unused security rules

--- locals.tf
+++ locals.tf
@@ -4,7 +4,6 @@ locals {
 
 locals {
   security_gruop_ingress_ec2_instance_web_server = [
-    ["SSH", 22, 22, "tcp", ["0.0.0.0/0"]],
     ["HTTP", 80, 80, "tcp", ["0.0.0.0/0"]],
     ["HTTPS", 443, 443, "tcp", ["0.0.0.0/0"]],
   ]
@@ -12,7 +11,6 @@ locals {
 
 locals {
   security_gruop_alb_web = [
-    [80, 80, "tcp", ["0.0.0.0/0"]],
     [443, 443, "tcp", ["0.0.0.0/0"]],
   ]
 }
